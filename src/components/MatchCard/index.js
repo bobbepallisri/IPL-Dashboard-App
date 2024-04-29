e93933238cd40ffb1d43c4587ccdf5407e29feed
bobbepallisri <srikanthbobbepalli3@gmail.com>
2024-04-29 13:17:49 +0000

    solution

---
+++ src/components/MatchCard/index.js
@@ -0,0 +1,27 @@
+// Write your code here
+// Write your code here
+import './index.css'
+
+const MatchCard = props => {
+  const {recentMatchDetails} = props
+  const {
+    competingTeamLogo,
+    competingTeam,
+    result,
+    matchStatus,
+  } = recentMatchDetails
+  return (
+    <li className="recent-match-card-container">
+      <img
+        className="recent-team-logo"
+        src={competingTeamLogo}
+        alt={`competing team ${competingTeam}`}
+      />
+      <p className="recent-team-name">{competingTeam}</p>
+      <p className="recent-match-result">{result}</p>
+      <p className="recent-match-status">{matchStatus}</p>
+    </li>
+  )
+}
+
+export default MatchCard
---
+++ src/components/NotFound/index.js
@@ -0,0 +1,13 @@
+// Write your code here
+import './index.css'
+
+const NotFound = () => {
+  const notFound = 'Page Not Found !!'
+  return (
+    <div className="not-found-container">
+      <h1 className="not-found-text">{notFound}</h1>
+    </div>
+  )
+}
+
+export default NotFound
